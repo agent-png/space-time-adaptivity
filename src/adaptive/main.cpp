2c27731b37e361022003ef6fd3d07d3e950ddc81
agent-png <giacomomaglio2803@gmail.com>
2026-03-12 12:48:52 +0100

    feat: sample of main with some parameters for simple tests

--- src/adaptive/main.cpp
+++ src/adaptive/main.cpp
@@ -3,5 +3,35 @@
 int
 main(int argc, char *argv[])
 {
-    return 0;
+  constexpr unsigned int dim = AdaptiveHeat::dim;
+
+  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
+
+  const auto mu = [](const Point<dim> & /*p*/) { return 1.0; };
+  double a = 1.5; 
+  int N = 3; 
+  Point<dim> x0(0, 0, 0); 
+  double sigma = 0.5; 
+  const auto g  = [&]( const double  &t) {
+    return (std::exp(-a * (std::cos(2*N*M_PI*t) + 1)));
+  };
+  const auto h = [&]( const Point<dim> &p) {
+    return std::exp(-((p-x0)*(p-x0)/std::pow(sigma,2)));
+  };
+
+  const auto f  = [&](const Point<dim>  &p, const double  &t) {
+    return g(t)*h(p);
+  };
+
+  Heat problem(/*mesh_filename = */ "../mesh/mesh-cube-20.msh",
+               /* degree = */ 1,
+               /* T = */ 1.0,
+               /* theta = */ 0.5,
+               /* delta_t = */ 0.05,
+               mu,
+               f);
+
+  problem.run();
+
+  return 0;
 }
